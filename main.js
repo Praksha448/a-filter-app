e62c88ea7b64a2c0ea765d5e196f5fa4e9ebe51b
Praksha448 <101506562+Praksha448@users.noreply.github.com>
2022-12-08 16:56:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() {
+    
+}
+function setup() {
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+
+    tint_colour="";
+    
+}
+function draw() {
+image(video,0,0,640,480);
+tint(tint_colour);
+}
+function filter_tint() {
+tint_colour=document.getElementById("color").value;
+}
+function Take_snapshot() {
+save('Student_name.png');
+}
